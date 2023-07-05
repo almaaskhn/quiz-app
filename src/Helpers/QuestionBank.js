0123ca7d088bf678de706e8a5f985fd9e70517bf
Almaas <almaaskn@gmail.com>
2023-07-05 10:55:00 +0530

    first commit

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,26 @@
+export const Questions = [
+{
+prompt: "What is 1+3?",
+optionA: "24",
+optionB: "21",
+optionC: "4",
+optionD: "2",
+answer: "C",
+},
+{
+    prompt: "What is 11*11?",
+    optionA: "0",
+    optionB: "1",
+    optionC: "100",
+    optionD: "121",
+    answer: "D",
+    },
+    {
+        prompt: "What is 12/2?",
+        optionA: "24",
+        optionB: "6",
+        optionC: "4",
+        optionD: "2",
+        answer: "B",
+        },
+];
